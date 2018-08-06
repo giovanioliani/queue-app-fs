d6989c5c2861f59647ed1c37251d669822da7aa1
Giovani Granzotto <giovani@localhost.localdomain>
2018-08-06 16:02:31 -0300

    add newCall method, add getIdentifier method

--- client.py
+++ client.py
@@ -7,8 +7,8 @@ from twisted.internet import stdio
 from twisted.protocols.basic import LineReceiver
 from sys import stdout
 
-CONNECT_STRING = "connect\n\nevents text all\n\nfilter Event-Name\
-                 CHANNEL_CREATE CHANNEL_ANSWER CHANNEL_HANGUP\n\n"
+CONNECT_STRING = "connect\n\nevents text all\n\nfilter Event-Name"\
+                 "CHANNEL_CREATE\n\n"
 
 # class CommandReader(LineReceiver):
 #     delimiter = '\n'
@@ -42,24 +42,47 @@ class FreeSwitchInterfaceProtocol(Protocol):
         self.transport.write(CONNECT_STRING)
 
     def dataReceived(self, data):
-        print data[12:20]
+        print data[12:data.find("\n\n")]
         #self.queueConnector.transport.write(json.dumps(dict(command="call",id="1")))
 
+    def getIdentifier(self, event):
+        """
+        I return the unique call string identifier
+        """
+        # get start of string
+        start = event.find("Unique-ID: ")
+
+        # return id
+        return event[start::event[start::].find("\n")]
     def do_call(self, data):
-        return
+        #### put commands to send "originate bridge" here
+        self.parent.newCall(getIdentifier(data))
+        self.queueConnector.transport.write(json.dumps(dict("call",
+                                            self.parent.callNumber-1)))
+
     def do_answer(self):
+        #### commands to detect CHANNEL_ANSWER event and call server
         return
     def do_reject(self):
+        #### commands to detect CHANNEL_HANGUP (call state ringing) event and
+        #### call server
         return
     def do_hangup(self):
+        #### commands to detect CHANNEL_HANGUP (call state active)event and
+        #### call server
         return
 
 class FreeSwitchInterfaceFactory(ClientFactory):
     def __init__(self):
         self.commandFactory = CommandClientFactory()
+        # maps unique call identifiers into call numbers
         self.calls = {}
         self.callNumber = 1
-        
+    
+    def newCall(self, uid):
+        self.calls[uid] = self.callNumber
+        self.callNumber = self.callNumber + 1
+
     def buildProtocol(self, addr):
         return FreeSwitchInterfaceProtocol(self)
 
